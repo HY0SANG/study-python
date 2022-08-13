bd16243fbe50383c0bab7d121d19a545c500bca4
Kim Hyo Sang <110414297+HY0SANG@users.noreply.github.com>
2022-08-13 21:32:27 +0900

    Update numpy_reshape.py

--- study-python-numpy/numpy_reshape.py
+++ study-python-numpy/numpy_reshape.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-print("array")
-array = np.arange(8)
-print(array)
-print("shape : ", array.shape, "\n")
+print("array")  # array
+array = np.arange(8)  # array([0, 1, 2, 3, 4, 5, 6, 7])
+print(array)  # [0 1 2 3 4 5 6 7]
+print("shape : ", array.shape, "\n")  # shape :  (8,)
 
 # Q1. array를 (2,4) 크기로 reshape하여 matrix에 저장한 뒤 matrix와 그의 shape를 출력해보세요.
-print("# reshape (2, 4)")
-matrix = array.reshape(2, 4)
+print("# reshape (2, 4)")  # # reshape (2, 4)
+matrix = array.reshape(2, 4)  
 matrix = np.reshape(array, (2, 4))
-
+# array([[0, 1, 2, 3],
+#        [4, 5, 6, 7]])
 
 print(matrix)
+# [[0 1 2 3]
+# [4 5 6 7]]
 print("shape : ", matrix.shape)
+# shape :  (2, 4)
