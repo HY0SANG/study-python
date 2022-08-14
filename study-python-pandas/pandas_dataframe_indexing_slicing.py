2e54997a6b2d5802d199caa2adff5c934ff2ef79
Kim Hyo Sang <110414297+HY0SANG@users.noreply.github.com>
2022-08-14 18:08:20 +0900

    Create pandas_dataframe_indexing_slicing.py

---
+++ study-python-pandas/pandas_dataframe_indexing_slicing.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+# 첫번째 컬럼을 인덱스로 country.csv 파일 읽어오기.
+print("Country DataFrame")
+country = pd.read_csv("./data/country.csv", index_col=0)
+print(country, "\n")
+
+# 명시적 인덱싱을 사용하여 데이터프레임의 "china" 인덱스를 출력해봅시다.
+print(country.loc['china'])
+
+
+# 정수 인덱싱을 사용하여 데이터프레임의 1번째부터 3번째 인덱스를 출력해봅시다.
+print(country.iloc[1:4])
